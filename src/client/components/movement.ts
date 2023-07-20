62fed4f2e1948f388b5e1e863124e47d8dad9e8e
WorshipMeOrElse <sylviaviajung@gmail.com>
2023-07-19 19:38:09 -0700

    ok.. false alarm.. NOW the movement system works

--- src/client/components/movement.ts
+++ src/client/components/movement.ts
@@ -1,66 +1,78 @@
-import { Workspace, ContextActionService, RunService } from "@rbxts/services"
+import { Workspace, ContextActionService, RunService } from "@rbxts/services";
 import { BaseComponent, Component } from "@flamework/components";
 import { OnStart } from "@flamework/core";
-import { promiseR6 } from "@rbxts/promise-character"
-import { Character } from "client/controllers/character";
+import { promiseR6 } from "@rbxts/promise-character";
 
 interface Attributes {
-  dashSpeed: number
-  walkSpeed: number
-  dashCooldown: number
+	dashSpeed: number;
+	walkSpeed: number;
+	dashCooldown: number;
 }
 
 @Component({
-  defaults: {
-    dashSpeed: 24,
-    walkSpeed: 24,
-    dashCooldown: 0.25
-  },
-  refreshAttributes: false
+	defaults: {
+		dashSpeed: 24,
+		walkSpeed: 24,
+		dashCooldown: 0.25,
+	},
+	refreshAttributes: false,
 })
-
 export class Movement extends BaseComponent<Attributes, Model> implements OnStart {
-  private mass = promiseR6(this.instance)
-    .then((character) => character.GetChildren()
-      .filter((part): part is BasePart => part.IsA("BasePart"))
-      .reduce((sum, part) => sum + part.GetMass(), 0)).expect()
+	private mass = promiseR6(this.instance)
+		.then((character) =>
+			character
+				.GetChildren()
+				.filter((part): part is BasePart => part.IsA("BasePart"))
+				.reduce((sum, part) => sum + part.GetMass(), 0),
+		)
+		.expect();
 
-  private canDash = true
+	private canDash = true;
 
-  private async Dash() {
-    if (!this.canDash) return;
-    this.canDash = false
+	private async Dash() {
+		if (!this.canDash) return;
+		this.canDash = false;
 
-    const character = await promiseR6(this.instance)
-    const humanoid = character.Humanoid
-    const camera = Workspace.CurrentCamera
+		const character = await promiseR6(this.instance);
+		const humanoid = character.Humanoid;
+		const camera = Workspace.CurrentCamera;
 
-    if (humanoid.Sit) { this.canDash = true; return; }
-    if (!camera) { this.canDash = true; return; }
+		if (humanoid.Sit) {
+			this.canDash = true;
+			return;
+		}
+		if (!camera) {
+			this.canDash = true;
+			return;
+		}
 
-    const rootpart = character.HumanoidRootPart
+		const rootpart = character.HumanoidRootPart;
 
-    humanoid.Sit = true
+		humanoid.Sit = true;
 
-    const [x, _y, _z] = camera.CFrame.ToOrientation()
-    const direction = CFrame.fromAxisAngle(Vector3.xAxis, x).VectorToWorldSpace(humanoid.MoveDirection)
-    rootpart.ApplyImpulse(direction.mul(this.mass * this.attributes.dashSpeed))
+		const [x, _y, _z] = camera.CFrame.ToOrientation();
 
-    task.wait(this.attributes.dashCooldown)
-    this.canDash = true
-  }
+		const rootpartPosition = rootpart.Position;
+		const rp = rootpartPosition;
 
-  async onStart() {
-    const character = await promiseR6(this.instance)
-    const humanoid = character.Humanoid
-    humanoid.WalkSpeed = this.attributes.walkSpeed
+		const direction = CFrame.lookAt(rp, rp.add(humanoid.MoveDirection)).mul(CFrame.Angles(x, 0, 0)).LookVector;
+		rootpart.ApplyImpulse(direction.mul(this.mass * this.attributes.dashSpeed));
 
-    ContextActionService.BindAction("Dash", () => this.Dash(), true, Enum.KeyCode.E, Enum.KeyCode.ButtonB)
-    ContextActionService.SetTitle("Dash", "Dash")
-    ContextActionService.SetPosition("Dash", new UDim2(1, -70, 0, 10))
-  }
+		task.wait(this.attributes.dashCooldown);
+		this.canDash = true;
+	}
 
-  public Destroy() {
-    ContextActionService.UnbindAction("Dash")
-  }
+	async onStart() {
+		const character = await promiseR6(this.instance);
+		const humanoid = character.Humanoid;
+		humanoid.WalkSpeed = this.attributes.walkSpeed;
+
+		ContextActionService.BindAction("Dash", () => this.Dash(), true, Enum.KeyCode.E, Enum.KeyCode.ButtonB);
+		ContextActionService.SetTitle("Dash", "Dash");
+		ContextActionService.SetPosition("Dash", new UDim2(1, -70, 0, 10));
+	}
+
+	public Destroy() {
+		ContextActionService.UnbindAction("Dash");
+	}
 }
